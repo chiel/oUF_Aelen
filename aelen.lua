1a911b49693aaf46ece8ede7672f517d4338de44
Chiel Kunkels <chiel@kunkels.me>
2014-05-23 01:17:10 +0200

    Simplified anchoring and removed debug statements

--- aelen.lua
+++ aelen.lua
@@ -21,10 +21,6 @@ local Player = function(self, unit)
 	self.colors = colors
 	self:SetSize(214, 26)
 
-	local classDisplayName, class, classID = UnitClass('player')
-	print(classDisplayName, class, classID)
-	print(colors)
-
 	-- health
 	local HealthBg = CreateFrame('Frame', 'nil', self)
 	HealthBg:SetHeight(17)
@@ -38,10 +34,8 @@ local Player = function(self, unit)
 	local Health = CreateFrame('StatusBar', nil, HealthBg)
 	Health:SetStatusBarTexture(_TEXTURE)
 	Health:SetStatusBarColor(.15, .15, .15, 1)
-	Health:SetPoint('TOP', 0, -1)
-	Health:SetPoint('LEFT', 1, 0)
-	Health:SetPoint('RIGHT', -1, 0)
-	Health:SetPoint('BOTTOM', 0, 1)
+	Health:SetPoint('TOPLEFT', 1, -1)
+	Health:SetPoint('BOTTOMRIGHT', -1, 1)
 	Health.frequentUpdates = true
 	Health.colorDisconnected = true
 	Health.colorTapping = true
@@ -59,10 +53,8 @@ local Player = function(self, unit)
 
 	local Power = CreateFrame('StatusBar', nil, PowerBg)
 	Power:SetStatusBarTexture(_TEXTURE)
-	Power:SetPoint('TOP', 0, -1)
-	Power:SetPoint('LEFT', 1, 0)
-	Power:SetPoint('RIGHT', -1, 0)
-	Power:SetPoint('BOTTOM', 0, 1)
+	Power:SetPoint('TOPLEFT', 1, -1)
+	Power:SetPoint('BOTTOMRIGHT', -1, 1)
 	Power.colorPower = true
 	Power.frequentUpdates = true
 	self.Power = Power
